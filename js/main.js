ab2696a9cc08fcf77535f2e4759ded4341eb7b98
AdamJacobson <amartenj@gmail.com>
2017-07-03 15:37:05 -0700

    Complete TTT game

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const Game = require("./game.js");
+const View = require("./ttt-view.js");
 
 $( () => {
-  // Your code here
+  // <figure class="ttt"></figure>
+  const $container = $('figure.ttt');
+  let game = new Game();
+  let view = new View(game, $container);
+  view.setupBoard();
 });
